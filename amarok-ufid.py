eaa749e83aa493f37c981cd710c70465d7cf750a
Benedikt Waldvogel <mail@bwaldvogel.de>
2013-10-12 15:55:49 +0200

    cleanup

--- amarok-ufid.py
+++ amarok-ufid.py
@@ -2,20 +2,25 @@
 # vim: set fileencoding=utf-8 ts=4 sw=4 et :
 # written by Benedikt Waldvogel
 
-import __future__
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
 
-import getopt, sys, os, re, mimetypes, logging
-from stat import *
+import getopt
+import sys
+import os
+import re
+import mimetypes
+import logging
+# from stat import *
 
-scriptname = os.path.basename(sys.argv[0])
-ufidFile = "ufid.dump"
-directory = u"."
-force = False
-notify = False
-filePattern = re.compile(r".+\.(flac|mp3|ogg)")
+
+def get_scriptname():
+    return os.path.basename(sys.argv[0])
+
 
 # init logging
-logger = logging.getLogger("simple_example")
+logger = logging.getLogger(get_scriptname())
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
@@ -27,188 +32,211 @@ logger.addHandler(ch)
 try:
     import mutagen
 except ImportError, err:
-    logger.critical("%s depends on mutagen: %s" % (scriptname, str(err)))
+    logger.critical("failed to import mutagen: %s" % (str(err)))
     sys.exit(1)
 
-def dump():
-    if (not force and os.path.exists(ufidFile)):
-        logger.error("file '%s' already exists. use -f to force it." % (ufidFile))
+
+def parse_line(line):
+    line_pattern = re.compile(r"^([0-9a-f-]{36}) maps to '(.+)' ([0-9a-f]{32}) \((.+)\)$")
+    match = line_pattern.match(line)
+    if not match:
+        logger.critical("unable to parse line '%s'" % (line))
         sys.exit(1)
 
-    out = None
+    musicbrainz_ufid = match.group(1)
+    amarok_owner = match.group(2)
+    amarok_data = match.group(3)
+    path = match.group(4)
+
+    return musicbrainz_ufid, amarok_owner, amarok_data, path
+
+
+def dump(directory, ufid_file, force, notify, file_pattern):
+
+    if os.path.exists(ufid_file) and not force:
+        logger.error("file '%s' already exists. use -f to force it." % ufid_file)
+        sys.exit(1)
 
     files = os.listdir(directory)
     files.sort()
-    for path in files:
-        if (os.path.isdir(path)):
-            logger.warn("skipping '%s'" % (path))
-            continue
 
-        fm = mutagen.File(path)
+    with open(ufid_file, "w") as out:
+        for path in files:
+            if os.path.isdir(path):
+                logger.warn("skipping '%s'" % (path))
+                continue
 
-        if (filePattern.match(path) is None):
-            continue
+            fm = mutagen.File(path)
 
-        mimetype = mimetypes.guess_type(path)[0]
-        if (mimetype == "application/x-flac"):
-            musicbrainzUFID = fm["musicbrainz_trackid"][0]
-            ufidOwner = 'Amarok 2 AFTv1 - amarok.kde.org'
-            ufidData = fm["amarok 2 aftv1 - amarok.kde.org"][0]
-        elif (mimetype == "audio/mpeg"):
-            musicbrainzUFID = fm["UFID:http://musicbrainz.org"].data
-            amarokUFID = fm.get("UFID:Amarok 2 AFTv1 - amarok.kde.org")
-            ufidOwner = amarokUFID.owner
-            ufidData = amarokUFID.data
-        elif (mimetype == "audio/ogg"):
-            musicbrainzUFID = fm["musicbrainz_trackid"][0]
-            ufidOwner = 'Amarok 2 AFTv1 - amarok.kde.org'
-            ufidData = fm.get("amarok 2 aftv1 - amarok.kde.org")[0]
-        else:
-            raise Exception("unknown mimtype %s" % (mimetype))
+            if not file_pattern.match(path):
+                continue
 
-        logger.debug(" %-50s %s" % (path, musicbrainzUFID))
-        line = musicbrainzUFID + " maps to '" + ufidOwner + "' " + ufidData + " (" + path + ")"
+            mimetype, _ = mimetypes.guess_type(path)
+            if mimetype in ("application/x-flac", "audio/x-flac"):
+                musicbrainz_ufid, = fm["musicbrainz_trackid"]
+                ufid_owner = 'Amarok 2 AFTv1 - amarok.kde.org'
+                ufid_data, = fm["amarok 2 aftv1 - amarok.kde.org"]
+            elif mimetype == "audio/mpeg":
+                musicbrainz_ufid = fm["UFID:http://musicbrainz.org"].data
+                amarok_ufid = fm.get("UFID:Amarok 2 AFTv1 - amarok.kde.org")
+                ufid_owner = amarok_ufid.owner
+                ufid_data = amarok_ufid.data
+            elif mimetype == "audio/ogg":
+                musicbrainz_ufid, = fm["musicbrainz_trackid"]
+                ufid_owner = 'Amarok 2 AFTv1 - amarok.kde.org'
+                ufid_data, = fm.get("amarok 2 aftv1 - amarok.kde.org")
+            else:
+                raise Exception("unknown mimetype: %s" % (mimetype))
 
-        # lazy open
-        if (out is None):
-            out = open(ufidFile, "w")
-        out.write(line.encode("utf-8") + "\n")
+            assert musicbrainz_ufid and ufid_owner and ufid_data
 
-    if (out):
-        out.close();
+            logger.debug(" %-50s %s" % (path, musicbrainz_ufid))
+            data = (musicbrainz_ufid, ufid_owner, ufid_data, path)
+            line = u"%s maps to '%s' %s (%s)" % data
+            assert parse_line(line) == data
 
-    logger.info("done writing '%s'. copy the file to the target directory and call '%s apply'" % (ufidFile, scriptname))
+            out.write(line.encode("utf-8") + "\n")
 
-    if (notify):
-        os.system("notify-send \"" + scriptname + "\" \"finished dump\"")
+    logger.info("done writing '%s'. copy the file to the target directory and run with the command 'apply'" % ufid_file)
+
+    if notify:
+        os.system("notify-send \"%s\" \"finished dump\"" % get_scriptname())
 
 
-def apply():
+def read_ufid_file(ufid_file):
+    with open(ufid_file, "r") as fileIn:
+
+        ufids = {}
+        for line in fileIn:
+
+            musicbrainz_ufid, amarok_owner, amarok_data, _ = parse_line(line)
+
+            if musicbrainz_ufid in ufids:
+                logger.critical("found duplicate UFID: '%s'" % (musicbrainz_ufid))
+                sys.exit(1)
+
+            ufids[musicbrainz_ufid] = (amarok_owner, amarok_data)
+
+        return ufids
+
+
+def apply(directory, ufid_file, force, notify, file_pattern):
     files = os.listdir(directory)
     files.sort()
 
-    fileIn = open(ufidFile, "r")
-
-    ufids = {}
-
-    linePattern = re.compile(r"^([0-9a-f-]{36}) maps to '(.+)' ([0-9a-f]{32}) \(.+\)$")
-    for line in fileIn.readlines():
-        match = linePattern.match(line)
-        if (match is None):
-            logger.critical("unable to parse line '%s'" % (line))
-            sys.exit(1)
-
-        musicbrainzUFID = match.group(1)
-        amarokOwner = match.group(2)
-        amarokData = match.group(3)
-
-        if (musicbrainzUFID in ufids):
-            logger.critical("found duplicate UFID: '%s'" % (musicbrainzUFID))
-            sys.exit(1)
-
-        ufids[musicbrainzUFID] = (amarokOwner, amarokData)
-
-    fileIn.close()
+    ufids = read_ufid_file(ufid_file)
 
     for f in files:
-        match = filePattern.match(f)
-        if (match is None or os.path.isdir(f)):
+        match = file_pattern.match(f)
+        if not match or os.path.isdir(f):
             logger.warn("skipping '%s'" % (f))
             continue
 
-        mimetype = mimetypes.guess_type(f)[0]
-        if (mimetype != "application/x-flac"):
+        mimetype, _ = mimetypes.guess_type(f)
+        if mimetype not in ("application/x-flac", "audio/x-flac"):
             logger.error("%s: unsupported mimetype: '%s'. applying UFIDs is only supported for FLAC" % (f, mimetype))
             sys.exit(1)
 
         tags = mutagen.File(f)
 
         try:
-            musicbrainzUFID = tags['musicbrainz_trackid'][0]
+            musicbrainz_ufid, = tags['musicbrainz_trackid']
         except:
             logger.error("'%s' has no MusicBrainz track id. was the file properly tagged with picard?" % (f))
             sys.exit(1)
 
-        if (not musicbrainzUFID in ufids):
+        if not musicbrainz_ufid in ufids:
             logger.error("no UFID mapping for '%s'" % (f))
             sys.exit(1)
 
-        amarokUFID = ufids[musicbrainzUFID]
+        ufid_owner, ufid_data = ufids[musicbrainz_ufid]
 
-        ufidOwner = amarokUFID[0]
-        ufidData = amarokUFID[1]
+        logger.debug("adding UFID %s to '%s'" % (ufid_data, f))
 
-        logger.debug("adding UFID %s to '%s'" % (ufidData, f))
+        if force or ufid_owner not in tags:
+            tags[ufid_owner] = ufid_data
+            tags.save()
+        else:
+            existing_ufid, = tags[ufid_owner]
+            if existing_ufid != ufid_data:
+                logger.error("different ufid already exists: '%s' use -f to force overwriting." % existing_ufid)
+                sys.exit(1)
+            else:
+                logger.info("ufid for '%s' already set. skipping" % f)
 
-        if (not force and ufidOwner in tags):
-            logger.error("owner already exists. use -f to force overwriting.")
-            sys.exit(1)
+        # remember that we processed that ufid
+        del ufids[musicbrainz_ufid]
 
-        tags[ufidOwner] = ufidData
-        tags.save()
-
-        del(ufids[musicbrainzUFID])
-
-
-    if (len(ufids) > 0):
+    if ufids:
         logger.error("unmatched UFIDs: %s" % (str(ufids)))
         sys.exit(1)
 
     logger.info("done")
-    if (notify):
-        os.system("notify-send \"" + scriptname + "\" \"finished apply\"")
+    if notify:
+        os.system("notify-send \"%s\" \"finished apply\"" % get_scriptname())
 
 
-def usage():
-    print "usage: " + scriptname + " [OPTION] COMMAND"
-    print ""
-    print "OPTION:"
-    print "   -o, --file=FILE   dump file (default: " + ufidFile + ")"
-    print "   -f, --force       force overwriting existing data"
-    print "   -v                be verbose"
-    print "   -n                notify"
-    print "   -h, --help        print the help and exit"
-    print ""
-    print "COMMAND:"
-    print "   dump    dump the UFIDs to the file '" + ufidFile + "'"
-    print "   apply   apply the UFIDs from the file '" + ufidFile + "'"
-    print ""
+def usage(ufid_file_default):
+    print("usage: %s [OPTIONS] COMMAND" % get_scriptname())
+    print("")
+    print("OPTION:")
+    print("   -d, --dump=FILE   dump file (default: %s)" % ufid_file_default)
+    print("   -f, --force       force overwriting existing data")
+    print("   -v                be verbose")
+    print("   -n                notify")
+    print("   -h, --help        print the help and exit")
+    print("")
+    print("COMMAND:")
+    print("   dump    dump the UFIDs to the dump file")
+    print("   apply   apply the UFIDs from the dump file")
+    print("")
 
-def main():
-    global force, ufidFile, notify
+
+def main(directory):
+
+    ufid_file_default = 'ufid.dump'
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hd:fvn", ["help", "dump=", "force", "notify"])
     except getopt.GetoptError, err:
-        print str(err) # will print something like "option -a not recognized"
-        usage()
+        print("%s" % str(err), file=sys.stderr)  # will print something like "option -a not recognized"
+        print("", file=sys.stderr)
+        usage(ufid_file_default)
         sys.exit(2)
+
+    ufid_file = ufid_file_default
+    force = False
+    notify = False
 
     for o, a in opts:
         if o == "-v":
             ch.setLevel(logging.DEBUG)
         elif o in ("-h", "--help"):
-            usage()
+            usage(ufid_file_default)
             sys.exit(1)
         elif o in ("-d", "--dump"):
-            ufidFile = a
+            ufid_file = a
         elif o in ("-f", "--force"):
             force = True
         elif o in ("-n", "--notify"):
             notify = True
         else:
-            assert False, "unhandled option"
+            assert False, "unknown option: %s" % o
 
-    if (len(args) != 1):
-        usage()
+    if len(args) != 1:
+        print("unexpected number of arguments: %d" % len(args), file=sys.stderr)
+        usage(ufid_file_default)
         sys.exit(2)
 
-    if (args[0] == "dump"):
-        dump()
-    elif (args[0] == "apply"):
-        apply()
+    file_pattern = re.compile(r".+\.(flac|mp3|ogg)")
+
+    command = args[0]
+    if command == "dump":
+        dump(directory, ufid_file, force, notify, file_pattern)
+    elif command == "apply":
+        apply(directory, ufid_file, force, notify, file_pattern)
     else:
-        assert False, "invalid command: " + args[0]
+        assert False, "invalid command: %s" % command
 
 if __name__ == "__main__":
-    main()
+    main(u".")
